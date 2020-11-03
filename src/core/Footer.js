f82f1b4d96d03602e3faccb6c29fd571d4a75b53
ashutosh1401 <ashutoshthakur_14@yahoo.com>
2020-11-03 19:18:22 +0530

    Footer

--- src/core/Footer.js
+++ src/core/Footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import image1 from '../images/index.jpg'
 
 const Footer = () => {
     return (
@@ -6,60 +7,75 @@ const Footer = () => {
             <footer className="page-footer font-small blue pt-4">
 
                 <div className="container-fluid text-center text-md-center">
-                    <div className="row row-bg">
-                    <div className="col-md-3 mb-md-0 mb-3">
+                    <div className="row row-bg">                   
+                    <div className="col-md-4 mt-md-0 mt-3">
+                        <div className="link-row">
+                            <ul className="link-unstyled link-color">
+                                <li><i></i></li>
+                                <li><i></i></li>
+                                <li><i></i></li>
+                                <li><i></i></li>
+                                <li><i></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-4 mb-md-0 mb-3">
 
-                        <h5 className="text-uppercase">Links</h5>
-
-                        <ul className="list-unstyled">
+                        <h5 className="text-uppercase"><b>Footer Links</b></h5>
+                        <br />
+                        <ul className="list-unstyled link-color text-left">
                         <li>
-                            <a href="#!">Link 1</a>
+                            <a href="#!">Home</a>
                         </li>
+                        <br />
                         <li>
-                            <a href="#!">Link 2</a>
+                            <a href="#!">Privacy & Policy</a>
                         </li>
+                        <br />
                         <li>
-                            <a href="#!">Link 3</a>
+                            <a href="#!">Terms & Condition</a>
                         </li>
+                        <br />
                         <li>
-                            <a href="#!">Link 4</a>
+                            <a href="#!">Partner Program</a>
+                        </li>
+                        <br />
+                        <li>
+                            <a href="#!">Contact Us</a>
                         </li>
                         </ul>
 
-                    </div>                    
-                    <div className="col-md-6 mt-md-0 mt-3">
-                        <h5 className="text-uppercase">Footer Content</h5>
-                        <p>Here you can use rows and columns to organize your footer content.</p>
-
-                    </div>
-                    
+                    </div> 
 
                     <hr className="clearfix w-100 d-md-none pb-3" />
 
                     
-                    <div className="col-md-3 mb-md-0 mb-3">
+                    <div className="col-md-4 mb-md-0 mb-3">
 
-                        <h5 className="text-uppercase">Links</h5>
+                        <h5 className="text-uppercase"><b>RECENT POST</b></h5>
 
-                        <ul className="list-unstyled">
+                        <ul className="list-unstyled link-color">
                         <li>
-                            <a href="#!">Link 1</a>
+                            <img src={image1} width="52px" height="52px" /><a href="#!">How to Design effective arts?</a><br/>
+                            <span>Jan 03 - 2019</span>
+                            <hr />
                         </li>
                         <li>
-                            <a href="#!">Link 2</a>
+                            <img src={image1} width="52px" height="52px" /><a href="#!">How to Design effective arts?</a><br/>
+                            <span>Jan 03 - 2019</span>
+                            <hr />
                         </li>
                         <li>
-                            <a href="#!">Link 3</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 4</a>
+                            <img src={image1} width="52px" height="52px" /><a href="#!">How to Design effective arts?</a><br/>
+                            <span>Jan 03 - 2019</span>
+                            <hr />
                         </li>
                         </ul>
 
                     </div>                    
                     </div>
-
-
+                    <div className="row" style={{backgroundColor: "black", width: "auto", height: "50px"}}>
+                    </div>
                 </div>
                 
             </footer>
