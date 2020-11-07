bd760262d5462b83567623fbc48848164a785175
ashutosh1401 <ashutoshthakur_14@yahoo.com>
2020-11-07 15:17:13 +0530

    Added More images and completed Saas compilation

--- src/core/Blog.js
+++ src/core/Blog.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react"
 import Layout from './Layout';
 import BlogCard from './BlogCard'
 import Search from './Search';
-import image1 from '../images/index.jpg'
+import image1 from '../images/image.jpg'
+import image2 from '../images/image2.jpg'
+import image3 from '../images/image3.jpg'
+import image4 from '../images/image4.jpg'
 import Pagination from '@material-ui/lab/Pagination';
 
 const Blog = () => {
@@ -12,13 +15,13 @@ const Blog = () => {
             <Search />
             <div className="grid-collection">
                 <BlogCard title="How to design effective arts?" img={image1} />
+                <BlogCard title="How to design effective arts?" img={image2} />
+                <BlogCard title="How to design effective arts?" img={image3} />
+                <BlogCard title="How to design effective arts?" img={image4} />
                 <BlogCard title="How to design effective arts?" img={image1} />
-                <BlogCard title="How to design effective arts?" img={image1} />
-                <BlogCard title="How to design effective arts?" img={image1} />
-                <BlogCard title="How to design effective arts?" img={image1} />
-                <BlogCard title="How to design effective arts?" img={image1} />
-                <BlogCard title="How to design effective arts?" img={image1} />
-                <BlogCard title="How to design effective arts?" img={image1} />
+                <BlogCard title="How to design effective arts?" img={image2} />
+                <BlogCard title="How to design effective arts?" img={image3} />
+                <BlogCard title="How to design effective arts?" img={image4} />
                 <BlogCard title="How to design effective arts?" img={image1} />
             </div>
             <div className="pagination-col">
